74178a4273631da7b2c0b31a513e34560fb74220
Ryan Dwyer <ryan@rydwy.com>
2024-08-26 16:57:31 -0700

    Prettier Test

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,13 +1,19 @@
 async function init() {
-    console.log("Hello, World!");
-    await sleep(1000);
-    console.log("Hello, World After Waiting!");
-  }
-  
-  function sleep(ms: number) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
+  console.log('Hello, World!');
+  await sleep(1000);
+  console.log('Hello, World After Waiting!');
+}
+
+function sleep(ms: number) {
+  const x = 'Hello';
+  let y = 'Hello!';
+
+  y += x;
+  console.log(y);
+
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
 init();
