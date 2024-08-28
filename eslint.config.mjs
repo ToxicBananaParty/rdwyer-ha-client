785ecfbc6e383baecfa6952150d1539ba3bf5c89
Ryan Dwyer <ryan@rydwy.com>
2024-08-27 17:45:56 -0700

    Lint the linter config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,30 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-
 export default [
-  { 
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    ignores: ["**/node_modules", '**/dist', '**/public'],
-    settings: {
-      react: {
-        version: "detect"
-      }
-    }
-  },
-  {
-    languageOptions: { 
-      globals: globals.browser 
-    }
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  eslintPluginPrettierRecommended
+    {
+        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+        ignores: ['**/node_modules', '**/dist', '**/public'],
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
+    {
+        languageOptions: {
+            globals: globals.browser,
+            parserOtions: {
+                tsconfigRootDir: __dirname,
+                project: ['./packages/*/tsconfig.json'],
+            },
+        },
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    eslintPluginPrettierRecommended,
 ];
