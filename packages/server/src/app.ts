5d845441cc89aefe43bc6387b90d52237ea4cc47
Ryan Dwyer <ryan@rydwy.com>
2024-09-14 14:31:38 -0700

    Replace numTwo test with displayMode feature

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -2,27 +2,52 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import { execSync } from 'child_process';
-import { baseCommand } from './utils.js';
+//import { baseCommand } from './utils.js';
 
 dotenv.config({
     path: `/mnt/c/Users/Ryan-PC/Projects/rdwyer-ha-client/.env`,
 });
 
 const app = express();
-const port = process.env.PORT || 6969;
+const port = process.env.BACKEND_PORT || 6969;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
+// TODO: Routing per module
 app.get('/', (req, res) => {
     res.send('root');
 });
 
-app.get('/numTwo', (req, res) => {
-    res.send('numTwo');
-    execSync(baseCommand);
+const getNumMonitors = (): number => {
+    const displayInfo = execSync('xrandr', { encoding: 'utf-8' }).toLowerCase();
+    //console.log(displayInfo);
+
+    const regex = displayInfo.match(/xwayland/g);
+    if (regex) {
+        return regex.length;
+    }
+
+    return -1;
+};
+
+app.get('/displayMode', (req, res) => {
+    res.send({ displayMode: getNumMonitors() });
+});
+
+app.post('/displayMode', (req, res) => {
+    const numMonitors = getNumMonitors();
+    if (numMonitors === 2) {
+        // Currently in Couch Mode -> Switch to One Monitor
+        res.send({ displayMode: 1 });
+        execSync('cmd.exe /C "displayswitch.exe /internal"');
+    } else if (numMonitors === 1) {
+        // Currently in Office Mode -> Switch to Two Monitors
+        res.send({ displayMode: 2 });
+        execSync('cmd.exe /C "displayswitch.exe /extend"');
+    }
 });
 
 app.listen(port, () => {
